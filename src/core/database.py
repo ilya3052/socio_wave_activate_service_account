0e1fb8b482a8b0c2c430286f9c33895c99b2218c
ilya3052 <ipopadin5@gmail.com>
2026-04-24 23:42:03 +0300

    создание файла подключения к базе данных

---
+++ src/core/database.py
@@ -0,0 +1,35 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.models import Base
+
+
+
+load_dotenv('src/core/cfg/.env')
+
+
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+DB_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# DB_URL = "sqlite:///src/db/example.db"
+
+engine = create_engine(
+    url=DB_URL,
+    # echo=True,
+    isolation_level='SERIALIZABLE'
+)
+
+Session = sessionmaker(engine)
+
+
+def create_tables():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
