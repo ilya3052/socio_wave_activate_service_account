51137b1d2c6e5e167193571443f6240103be408c
ilya3052 <ipopadin5@gmail.com>
2026-04-24 23:41:52 +0300

    создание файла конфигурации

---
+++ src/core/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+from dotenv import load_dotenv
+
+load_dotenv('cfg/.env')
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+KEY = os.getenv('ENCRYPTION_KEY')
+SESSION_FOLDER = os.getenv('SESSIONS_FOLDER')
+
+
+
