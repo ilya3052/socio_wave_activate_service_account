18ace3fa293a15437280f1b1d0fe959c91e821d0
ilya3052 <ipopadin5@gmail.com>
2026-04-24 23:44:26 +0300

    реализация конкретных репозиториев работы с объектами sqlalchemy

---
+++ src/repo/repository.py
@@ -0,0 +1,55 @@
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import joinedload
+
+from src.models import PlatformModel, ServiceAccountModel, ServiceAccountDataModel, UserModel, OneTimeActivateTokenModel
+from .base import BaseRepository
+
+
+class PlatformRepository(BaseRepository[PlatformModel]):
+    def __init__(self, session):
+        super().__init__(session, PlatformModel)
+
+
+class ServiceAccountRepository(BaseRepository[ServiceAccountModel]):
+    def __init__(self, session):
+        super().__init__(session, ServiceAccountModel)
+
+    def get_by_app_id(self, app_id):
+        try:
+            return self.session.scalars(
+                select(self.model).options(joinedload(self.model.data)).filter_by(app_id=app_id)
+            ).one_or_none()
+        except NoResultFound:
+            raise
+
+
+class ServiceAccountDataRepository(BaseRepository[ServiceAccountDataModel]):
+    def __init__(self, session):
+        super().__init__(session, ServiceAccountDataModel)
+
+    def get_by_phone_number(self, phone_number):
+        try:
+            return self.session.scalars(
+                select(self.model).options(joinedload(self.model.serviceAccount)).filter_by(phone_number=phone_number)
+            ).one_or_none()
+        except NoResultFound:
+            raise
+
+
+class UserRepository(BaseRepository[UserModel]):
+    def __init__(self, session):
+        super().__init__(session, UserModel)
+
+    def get_by_username(self, username):
+        try:
+            return self.session.scalars(
+                select(self.model).options(joinedload(self.model.one_time_token)).filter_by(username=username)
+            ).one_or_none()
+        except NoResultFound:
+            raise
+
+
+class OneTimeActivateTokenRepository(BaseRepository[OneTimeActivateTokenModel]):
+    def __init__(self, session):
+        super().__init__(session, OneTimeActivateTokenModel)
